ec19f30ea4db9e815f03d622c5e29c19b740982c
MatejVitovec <Matej.Vitovec@fs.cvut.cz>
2023-02-28 14:10:03 +0100

    pridani ukolu na nropdr

---
+++ NROPR/main.cpp
@@ -0,0 +1,51 @@
+#include <cmath>
+#include <iostream>
+#include <string>
+#include <fstream>
+
+double analytical(double x)
+{
+    double c = 1;
+    return c*std::exp(-2*x);
+}
+
+double F(double y)
+{
+    return -2*y;
+}
+
+double initCondition(double t)
+{
+    return 1.0;
+}
+
+double explicitEuler(double h, double start, double end)
+{
+    int n = int((end - start)/h);
+
+    double y = initCondition(start);
+
+    for (int i = 0; i < n; i++)
+    {
+        y += h*F(y);
+    }
+    
+    return y;
+}
+
+int main()
+{
+    std::ofstream writeToFile("results.txt");
+
+    for (int i = 1; i < 40; i++)
+    {
+        double h = (2.0/(pow(2.0,i)));
+        double result = explicitEuler(h, 0.0, 1.0);
+
+        double E = std::fabs(analytical(1.0) - result);
+
+        writeToFile << h << "," << E << std::endl;
+    }    
+
+    return 0;
+}
---
+++ NROPR/plot.py
@@ -0,0 +1,35 @@
+import sys
+from matplotlib import pyplot as plt
+
+inputFileName = "NROPR/results.txt"
+outputFileName = "NROPR/results.png"
+
+f = open(inputFileName, "r")
+res = []
+
+h = []
+E = []
+
+for line in f:
+    aux = line.split(",")
+    h.append(float(aux[0]))
+    E.append(float(aux[1]))
+
+f.close()
+
+
+
+plt.figure()
+
+plt.plot(h, E, "r-")
+plt.yscale("log")
+plt.xscale("log")
+plt.ylabel("E")
+plt.xlabel("h")
+
+plt.tight_layout()
+plt.savefig(outputFileName)
+
+print("Byl vytvořen soubor ", outputFileName)
+
+plt.close()
