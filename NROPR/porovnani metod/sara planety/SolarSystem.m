8d4683048f3b22ee515680b9520b1fbb7de46ea3
MatejVitovec <matejvitovec@gmail.com>
2023-05-01 22:07:35 +0200

    pridani objehu planet

--- NROPR/porovnani metod/sara planety/SolarSystem.m
+++ NROPR/porovnani metod/sara planety/SolarSystem.m
@@ -1,57 +1,137 @@
+clear all
 
-S = [0,0];
-Sv = [0,0];
-Sm = 1;
+global masses
+global G
+global numRows
+global numCols
 
-Z = [-0.1667743823220, 0.9690675883429];
-Zv = [-0.0172346557280, -0.0029762680930];
-Zm = 3.00348959632*10^(-6);
+data = fileread("data.txt");
+data = split(splitlines(data));
+[numRows,numCols] = size(data);
 
-M = [-0.1694619061456, 0.9692330175719];
-Mv = [-0.0172817331582, -0.0035325102831];
-Mm = (1.23000383*10^(-2))*Zm;
+names = string(data(2:numRows, 2));
+initialCondition = str2double(data(2:numRows, 3:8));
+masses = str2double(data(2:numRows, 9));
+planetNum = numRows - 1;
 
-t = 0;
-dt = 0.001;
+G = 6.67e-11;
 
-Zx = zeros(1, 100); %pro vykresleni
-Zy = zeros(1, 100); %pro vykresleni
-Mx = zeros(1, 100); %pro vykresleni
-My = zeros(1, 100); %pro vykresleni
-k = 0; %pro vykresleni
+tspan  = [0, 5000000000];
+[t, y] = ode78(@F, tspan, reshape(initialCondition',[],1));
 
+[yRows,yCols] = size(y);
 
-while t < 3
-    t = t + dt;
-    k = k + 1; %pro vykresleni
-    Za = (-pohybovaRovnice(S, Z, Sm, Zm)./Zm) + (-pohybovaRovnice(M, Z, Mm, Zm)./Zm);
-    Ma = (-pohybovaRovnice(S, M, Sm, Mm)./Mm) + (-pohybovaRovnice(Z, M, Zm, Mm)./Mm);
-    
-    Zv = Zv + dt.*Za;
-    Mv = Mv + dt.*Ma;
-    
-    Z = Z + dt.*Zv;
-    M = M + dt.*Mv;
-    
-    Zx(k) = Z(1); %pro vykresleni
-    Zy(k) = Z(2); %pro vykresleni
-    Mx(k) = M(1); %pro vykresleni
-    My(k) = M(2); %pro vykresleni
-end
+out = reshape(y, yRows, 6, planetNum);
 
-%pro vykresleni
+A(:,:) = out(yRows, :, :);
+
+zeme(:,:) = out(:, :, 4);
+
 figure
 hold on
-plot(Zx, Zy);
-plot(Mx, My);
-plot(0,0,'r*');
-hold off
 
-function [F] = pohybovaRovnice(r1, r2, m1, m2) %F vztahuje smer na 2 
-    G =  2.95912208286*10^(-4);
-    r = sqrt((r2(1)-r1(1))^2+(r2(2)-r1(2))^2);
-    r0 = [(r2(1)-r1(1))/r, (r2(2)-r1(2))/r];    
+plot(out(:, 1, 1), out(:, 2, 1));
+plot(out(:, 1, 2), out(:, 2, 2));
+plot(out(:, 1, 3), out(:, 2, 3));
+plot(out(:, 1, 4), out(:, 2, 4));
+plot(out(:, 1, 5), out(:, 2, 5));
+plot(out(:, 1, 6), out(:, 2, 6));
+plot(out(:, 1, 7), out(:, 2, 7));
+plot(out(:, 1, 8), out(:, 2, 8));
+plot(out(:, 1, 9), out(:, 2, 9));
+plot(out(:, 1, 10), out(:, 2, 10));
+
+% plot(initialCondition(1, 1), initialCondition(1, 2), "+");
+% plot(initialCondition(2, 1), initialCondition(2, 2), "+");
+% plot(initialCondition(3, 1), initialCondition(3, 2), "+");
+% plot(initialCondition(4, 1), initialCondition(4, 2), "+");
+% plot(initialCondition(5, 1), initialCondition(5, 2), "+");
+% plot(initialCondition(6, 1), initialCondition(6, 2), "+");
+% plot(initialCondition(7, 1), initialCondition(7, 2), "+");
+% plot(initialCondition(8, 1), initialCondition(8, 2), "+");
+% plot(initialCondition(9, 1), initialCondition(9, 2), "+");
+% plot(initialCondition(10, 1), initialCondition(10, 2), "+");
+
+legend(names);
+
+
+
+function [f] = F(~, u)
+    global masses
+    global G
+    global numRows
+    global numCols
+
+    planetNum = numRows - 1;
+
+    f = zeros(planetNum * 6, 1);
+
+    index = 1;
+
+    while index <= planetNum
     
-    F = G*((m1*m2)/(r^2));
-    F = F.*r0;
+        f(1+(index-1)*6) = u(4+(index-1)*6);
+        f(2+(index-1)*6) = u(5+(index-1)*6);
+        f(3+(index-1)*6) = u(6+(index-1)*6);
+        f(4+(index-1)*6) = 0;
+        f(5+(index-1)*6) = 0;
+        f(6+(index-1)*6) = 0;
+    
+        i = 1;
+        while i <= planetNum
+            if i == index
+                i = i + 1;
+                continue;
+            end
+            dist = distance(u((1+(i-1)*6):(3+(i-1)*6)), u((1+(index-1)*6):(3+(index-1)*6)));
+            distance3 = dist^3;
+            f(4+(index-1)*6) = f(4+(index-1)*6) + G*masses(i)*((u(1+(i-1)*6) - u(1+(index-1)*6))/distance3);
+            f(5+(index-1)*6) = f(5+(index-1)*6) + G*masses(i)*((u(2+(i-1)*6) - u(2+(index-1)*6))/distance3);
+            f(6+(index-1)*6) = f(6+(index-1)*6) + G*masses(i)*((u(3+(i-1)*6) - u(3+(index-1)*6))/distance3);
+            i = i + 1;
+        end
+        index = index + 1;
+    end
 end
+
+function [d] = distance(u1, u2)
+    d = sqrt((u1(1) - u2(1))^2 + (u1(2) - u2(2))^2 + (u1(3) - u2(3))^2);
+end
+
+
+
+
+% zaloha
+% function [f] = F(~, u)
+%     global masses
+%     global G
+%     f = zeros(11, 10);
+%     planetNum = 10;
+% 
+%     index = 1;
+% 
+%     while index <= planetNum
+%     
+%         f(1,index) = u(4, index);
+%         f(2,index) = u(5, index);
+%         f(3,index) = u(6, index);
+%         f(4,index) = 0;
+%         f(5,index) = 0;
+%         f(6,index) = 0;
+%     
+%         i = 1;
+%         while i <= planetNum
+%             if i == index
+%                 index = index + 1;
+%                 continue;
+%             end
+%             f(4,index) = f(4,index) + G*masses(i)*((u(1, i) - u(1, index))/distance(u(:, i), u(:, index)));
+%             f(5,index) = f(5,index) + G*masses(i)*((u(2, i) - u(2, index))/distance(u(:, i), u(:, index)));
+%             f(6,index) = f(6,index) + G*masses(i)*((u(3, i) - u(3, index))/distance(u(:, i), u(:, index)));
+%             i = i + 1;
+%         end
+%         index = index + 1;
+%     end
+% 
+%     f = f(:);
+% end
--- NROPR/porovnani metod/sara planety/SolarSystemOld.m
+++ NROPR/porovnani metod/sara planety/SolarSystemOld.m
@@ -1,29 +1,30 @@
+
 S = [0,0];
 Sv = [0,0];
-Sm = 27068700;
+Sm = 1;
 
-Z = [0,100];
-Zv = [50,0];
-Zm = 81.30;
+Z = [-0.1667743823220, 0.9690675883429];
+Zv = [-0.0172346557280, -0.0029762680930];
+Zm = 3.00348959632*10^(-6);
 
-M = [0, 100.1];
-Mv = [50,0];
-Mm = 1;
+M = [-0.1694619061456, 0.9692330175719];
+Mv = [-0.0172817331582, -0.0035325102831];
+Mm = (1.23000383*10^(-2))*Zm;
 
 t = 0;
 dt = 0.001;
 
-Zx = zeros(1, 10000);
-Zy = zeros(1, 10000);
-Mx = zeros(1, 10000);
-My = zeros(1, 10000);
-k = 0;
+Zx = zeros(1, 100); %pro vykresleni
+Zy = zeros(1, 100); %pro vykresleni
+Mx = zeros(1, 100); %pro vykresleni
+My = zeros(1, 100); %pro vykresleni
+k = 0; %pro vykresleni
 
 
-while t < 10
+while t < 365
     t = t + dt;
-    k = k+1;
-%     Za = (-pohybovaRovnice(S, Z, Sm, Zm)./Zm);
+    k = k + 1; %pro vykresleni
+
     Za = (-pohybovaRovnice(S, Z, Sm, Zm)./Zm) + (-pohybovaRovnice(M, Z, Mm, Zm)./Zm);
     Ma = (-pohybovaRovnice(S, M, Sm, Mm)./Mm) + (-pohybovaRovnice(Z, M, Zm, Mm)./Mm);
     
@@ -33,24 +34,49 @@ while t < 10
     Z = Z + dt.*Zv;
     M = M + dt.*Mv;
     
-    Zx(k) = Z(1);
-    Zy(k) = Z(2);
-    Mx(k) = M(1);
-    My(k) = M(2);
+    Zx(k) = Z(1); %pro vykresleni
+    Zy(k) = Z(2); %pro vykresleni
+    Mx(k) = M(1); %pro vykresleni
+    My(k) = M(2); %pro vykresleni
 end
 
+%pro vykresleni
 figure
 hold on
 plot(Zx, Zy);
 plot(Mx, My);
-
+plot(0,0,'r*');
 hold off
 
 function [F] = pohybovaRovnice(r1, r2, m1, m2) %F vztahuje smer na 2 
-    G = 6.674*10^(-11);
+    G =  2.95912208286*10^(-4);
     r = sqrt((r2(1)-r1(1))^2+(r2(2)-r1(2))^2);
     r0 = [(r2(1)-r1(1))/r, (r2(2)-r1(2))/r];    
     
     F = G*((m1*m2)/(r^2));
     F = F.*r0;
+end
+
+
+
+%TODO
+function [f] = func()
+    
+
+
+
+end
+
+
+function [u] = explicitEuler(maxIter, dt, u0, f)
+    u = u0;
+    un = u0;
+    iter = 0;
+
+    while iter <= maxIter
+        un = u + dt.*f;
+        u = un;
+        iter = iter + 1;
+    end
+
 end
